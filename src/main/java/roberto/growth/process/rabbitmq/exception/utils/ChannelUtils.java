198807ca3072efeb30985fe8f2b9f5cbc0389ea1
黄太洪 <robertohuang@foxmail.com>
2018-03-18 00:45:52 +0800

    添加RabbitMQ异常处理

---
+++ src/main/java/roberto/growth/process/rabbitmq/exception/utils/ChannelUtils.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (C), 2015-2018, ND Co., Ltd.
+ * FileName: ChannelUtils
+ * Author:   HuangTaiHong
+ * Date:     2018/3/17 23:47
+ * Description: RabbitMQ连接工具类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package roberto.growth.process.rabbitmq.exception.utils;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.impl.DefaultExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈RabbitMQ连接工具类〉
+ *
+ * @author HuangTaiHong
+ * @create 2018/3/17
+ * @since 1.0.0
+ */
+public class ChannelUtils {
+    public static Channel getChannelInstance(String connectionDescription) {
+        try {
+            ConnectionFactory connectionFactory = getConnectionFactory();
+            Connection connection = connectionFactory.newConnection(connectionDescription);
+            return connection.createChannel();
+        } catch (Exception e) {
+            throw new RuntimeException("获取Channel连接失败");
+        }
+    }
+
+    private static ConnectionFactory getConnectionFactory() {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+
+        connectionFactory.setHost("192.168.56.128");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+        connectionFactory.setUsername("roberto");
+        connectionFactory.setPassword("roberto");
+
+        connectionFactory.setAutomaticRecoveryEnabled(true);
+        connectionFactory.setNetworkRecoveryInterval(10000);
+
+        Map<String, Object> connectionFactoryPropertiesMap = new HashMap();
+        connectionFactoryPropertiesMap.put("principal", "RobertoHuang");
+        connectionFactoryPropertiesMap.put("description", "RGP订单系统V2.0");
+        connectionFactoryPropertiesMap.put("emailAddress", "RobertoHuang@foxmail.com");
+        connectionFactory.setClientProperties(connectionFactoryPropertiesMap);
+
+        // 设置自定义异常处理器
+        connectionFactory.setExceptionHandler(new DefaultExceptionHandler() {
+            @Override
+            public void handleConsumerException(Channel channel, Throwable exception, Consumer consumer, String consumerTag, String methodName) {
+                System.out.println("----------消息消费异常处理----------");
+                System.out.println("消息消费异常日志记录" + exception.getMessage());
+                super.handleConsumerException(channel, exception, consumer, consumerTag, methodName);
+            }
+        });
+        return connectionFactory;
+    }
+}
